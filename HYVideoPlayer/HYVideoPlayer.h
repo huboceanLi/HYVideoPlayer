ee822563d484d55110cb30b48793c35dae7e4d30
Ocean li <li437277219@gmail.com>
2023-09-28 21:19:52 +0800

    first commit

---
+++ HYVideoPlayer/HYVideoPlayer.h
@@ -0,0 +1,18 @@
+//
+//  HYVideoPlayer.h
+//  HYVideoPlayer
+//
+//  Created by Ocean 李 on 2023/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HYVideoPlayer.
+FOUNDATION_EXPORT double HYVideoPlayerVersionNumber;
+
+//! Project version string for HYVideoPlayer.
+FOUNDATION_EXPORT const unsigned char HYVideoPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HYVideoPlayer/PublicHeader.h>
+
+
